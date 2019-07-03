65d1a5414bc3a64d1477a307fb88729e67ddb582
Deianov <deqnow@gmail.com>
2019-07-03 23:04:10 +0300

    Inheritance - Exercises

--- src/C_Inheritance/Exercises/Animals/animals/Tomcat.java
+++ src/C_Inheritance/Exercises/Animals/animals/Tomcat.java
@@ -4,6 +4,10 @@ public class Tomcat extends Cat {
     private static final String ANIMAL_GENDER = "Male";
     private static final String ANIMAL_SOUND = "MEOW";
 
+    public Tomcat(String name, int age) {
+        super(name, age, ANIMAL_GENDER);
+    }
+
     public Tomcat(String name, int age, String gender) {
         super(name, age, ANIMAL_GENDER);
     }
